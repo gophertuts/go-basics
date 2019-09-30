698eeae080530b59c22f64758d1cdcdb8497d18a
Steve Hook <steevehook@gmail.com>
2019-09-30 10:36:38 +0300

    fix constants example

--- constants/overflow/main.go
+++ constants/overflow/main.go
@@ -5,18 +5,16 @@ import (
 	"math"
 )
 
-const (
-	Huge = 1.2676506002282294e+30
-)
+const Huge = 1.2676506002282294e+30
 
 func main() {
 	// int32
 	fmt.Println(^int32(math.Pow(2, 31))) // 2147483647
-	fmt.Println(int32(math.Pow(2, 31))) // -2147483648
+	fmt.Println(int32(math.Pow(2, 31)))  // -2147483648
 
 	// int64
 	fmt.Println(^int64(math.Pow(2, 63))) // -9223372036854775808
-	fmt.Println(int64(math.Pow(2, 63))) // 9223372036854775807
+	fmt.Println(int64(math.Pow(2, 63)))  // 9223372036854775807
 
 	// constants/overflow/main.go:11:13: constant 1267650600228229400000000000000 overflows int
 	fmt.Println(int(Huge))
