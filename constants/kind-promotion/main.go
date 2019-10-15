22a36390cd280151797729c08b7aa6d19bac4f3f
Steve Hook <steevehook@gmail.com>
2019-10-15 14:13:05 +0300

    fix comment in constants/kind-promotion

--- constants/kind-promotion/main.go
+++ constants/kind-promotion/main.go
@@ -9,7 +9,7 @@ type T complex128
 const (
 	t T = 1
 	// keep the order in mind, the last from this list has the top priority in the end result type
-	// integer, rune, floating-point, complex, custom type
+	// integer, rune, floating-point, complex, string, custom type
 	n     = 2 + t*'c'*2.0 + 35i
 	shift = 'c' << 5
 )
