38ffa1340a81aee60787a980d8c533f5a90f90e8
Steve Hook <steevehook@gmail.com>
2019-10-17 15:55:34 +0300

    update packages examples

--- packages/go-get/main.go
+++ packages/go-get/main.go
@@ -10,19 +10,11 @@ import (
 
 func main() {
 	router := httprouter.New()
-	router.GET("/", func(
-		w http.ResponseWriter,
-		req *http.Request,
-		_ httprouter.Params,
-	) {
+	router.GET("/", func(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
 		// handle errors bastard
 		_, _ = fmt.Fprintf(w, "Welcome!")
 	})
-	router.GET("/people/:name", func(
-		w http.ResponseWriter,
-		req *http.Request,
-		ps httprouter.Params,
-	) {
+	router.GET("/people/:name", func(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
 		name := ps.ByName("name")
 		// handle errors bastard
 		_, _ = fmt.Fprintf(w, "Welcome %s", name)
